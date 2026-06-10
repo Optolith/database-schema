fa89e8636f99d001c35b8a1ba7f601e4b80840a5
Lector <dragonsfire@gmx.de>
2026-06-10 20:50:24 +0200

    prettier

--- src/types/prerequisites/single/LanguagePrerequisite.ts
+++ src/types/prerequisites/single/LanguagePrerequisite.ts
@@ -3,19 +3,19 @@ import { LanguageIdentifier } from "../../_Identifier.js"
 //import { DisplayOption } from "../DisplayOption.js"
 
 export const LanguagePrerequisite = DB.TypeAlias(import.meta.url, {
-    name: "LanguagePrerequisite",
-    type: () =>
-        DB.Object({
-            id: DB.Required({
-                comment: "The languages’s identifier.",
-                type: LanguageIdentifier(),
-            }),
-            value: DB.Required({
-                comment: "The required minimum value.",
-                type: DB.Integer({ minimum: 0 }),
-            }),
-            //display_option: DB.Optional({
-            //    type: DB.IncludeIdentifier(DisplayOption),
-            //}),
-        }),
+  name: "LanguagePrerequisite",
+  type: () =>
+    DB.Object({
+      id: DB.Required({
+        comment: "The languagesï¿½s identifier.",
+        type: LanguageIdentifier(),
+      }),
+      value: DB.Required({
+        comment: "The required minimum value.",
+        type: DB.Integer({ minimum: 0 }),
+      }),
+      //display_option: DB.Optional({
+      //    type: DB.IncludeIdentifier(DisplayOption),
+      //}),
+    }),
 })
--- src/types/prerequisites/single/ScriptPrerequisite.ts
+++ src/types/prerequisites/single/ScriptPrerequisite.ts
@@ -3,15 +3,15 @@ import { ScriptIdentifier } from "../../_Identifier.js"
 //import { DisplayOption } from "../DisplayOption.js"
 
 export const ScriptPrerequisite = DB.TypeAlias(import.meta.url, {
-    name: "ScriptPrerequisite",
-    type: () =>
-        DB.Object({
-            id: DB.Required({
-                comment: "The scripts’s identifier.",
-                type: ScriptIdentifier(),
-            }),
-            //display_option: DB.Optional({
-            //    type: DB.IncludeIdentifier(DisplayOption),
-            //}),
-        }),
+  name: "ScriptPrerequisite",
+  type: () =>
+    DB.Object({
+      id: DB.Required({
+        comment: "The scriptsï¿½s identifier.",
+        type: ScriptIdentifier(),
+      }),
+      //display_option: DB.Optional({
+      //    type: DB.IncludeIdentifier(DisplayOption),
+      //}),
+    }),
 })
