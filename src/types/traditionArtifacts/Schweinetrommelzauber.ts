da1d2459db003a624cce4d84c2695b927d7476f0
Lector <dragonsfire@gmx.de>
2026-06-18 20:43:41 +0200

    feat: add schemas for goblin tradition artifact enchantments
    Refs: #163
    Closes #162
    Reviewed-by: Lukas Obermann <lukas.obermann@icloud.com>

---
+++ src/types/traditionArtifacts/Schweinetrommelzauber.ts
@@ -0,0 +1,36 @@
+import * as DB from "tsondb/schema/dsl"
+import { levels, maximum } from "../_Activatable.js"
+import { ap_value } from "../_ActivatableAdventurePointsValue.js"
+import { nameBuilderRules } from "../_ActivatableNames.js"
+import { cost, property, volume } from "../_ActivatableNonMundane.js"
+import { GeneralPrerequisites } from "../_Prerequisite.js"
+import { src } from "../source/_PublicationRef.js"
+import { translations } from "./_shared.js"
+
+export const Schweinetrommelzauber = DB.Entity(import.meta.url, {
+  name: "Schweinetrommelzauber",
+  namePlural: "Schweinetrommelzauber",
+  type: () =>
+    DB.Object({
+      levels,
+      nameBuilderRules,
+      maximum,
+      prerequisites: DB.Optional({
+        type: DB.IncludeIdentifier(GeneralPrerequisites),
+      }),
+      volume,
+      cost,
+      property: property(),
+      ap_value,
+      src,
+      translations: translations("Schweinetrommelzauber"),
+    }),
+  instanceDisplayName: {},
+  uniqueConstraints: [
+    {
+      entityMapKeyPath: "translations",
+      keyPathInEntityMap: "name_in_library",
+      keyPathInEntityMapFallback: "name",
+    },
+  ],
+})
