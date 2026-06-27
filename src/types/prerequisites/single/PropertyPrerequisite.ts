8c5f5fdaa9a1e5a2dbd881d66417d740b3fe54ee
Lector <dragonsfire@gmx.de>
2026-06-27 14:24:36 +0200

    prettier

--- src/types/prerequisites/single/PropertyPrerequisite.ts
+++ src/types/prerequisites/single/PropertyPrerequisite.ts
@@ -3,16 +3,16 @@ import { PropertyIdentifier } from "../../_Identifier.js"
 import { DisplayOption } from "../DisplayOption.js"
 
 export const PropertyPrerequisite = DB.TypeAlias(import.meta.url, {
-    name: "PropertyPrerequisite",
-    comment: "Requires a specific property or one of a specific set of properties.",
-    type: () =>
-        DB.Object({
-            id: DB.Required({
-                comment: "The property’s identifier.",
-                type: PropertyIdentifier(),
-            }),
-            display_option: DB.Optional({
-                type: DB.IncludeIdentifier(DisplayOption),
-            }),
-        }),
+  name: "PropertyPrerequisite",
+  comment: "Requires a specific property or one of a specific set of properties.",
+  type: () =>
+    DB.Object({
+      id: DB.Required({
+        comment: "The propertyï¿½s identifier.",
+        type: PropertyIdentifier(),
+      }),
+      display_option: DB.Optional({
+        type: DB.IncludeIdentifier(DisplayOption),
+      }),
+    }),
 })
